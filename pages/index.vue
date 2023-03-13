8ed0b087c90931d75c2ffb0babfe7a08d6c2db2e
lukaszflorczak <lukasz@florczak.dev>
2023-03-13 23:46:47 +0100

    feat: add more test phrases

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <h1>Index</h1>
+    <h1>Click the link, then refresh the website</h1>
 
-    <nuxt-link :to="url">Multiple Words with Spaces and Quotes</nuxt-link>
+    <nuxt-link :to="`/search?s=${phrase1}`">Single Word</nuxt-link>
+    <nuxt-link :to="`/search?s=${phrase2}`">Multiple Words with Spaces and Quotes</nuxt-link>
+    <nuxt-link :to="`/search?s=${phrase3}`">Phrase with `+` char</nuxt-link>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { normalizeURL } from 'ufo'
-
-  const phrase = '"Two Words"'
-  const url = normalizeURL(`/search?s=${phrase}`)
+  const phrase1 = 'Single'
+  const phrase2 = '"Two Words"'
+  const phrase3 = 'one+two'
 </script>
