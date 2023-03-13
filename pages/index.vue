fffd2c491d1e35000559cea462b71659be424c10
lukaszflorczak <lukasz@florczak.dev>
2023-03-14 00:14:40 +0100

    feat: add additional link

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,8 @@
       <li><nuxt-link :to="`/search?s=${phrase1}`">{{`/search?s=${phrase1}` }}</nuxt-link></li>
       <li><nuxt-link :to="`/search?s=${phrase2}`">{{ `/search?s=${phrase2}` }}</nuxt-link></li>
       <li><nuxt-link :to="`/search?s=${phrase3}`">{{ `/search?s=${phrase3}` }}</nuxt-link></li>
-      <li><nuxt-link to="/one+two">/one+two</nuxt-link></li>
+      <li><nuxt-link to="/one+two">/one+two (<code>~/pages/one two.vue</code>)</nuxt-link></li>
+      <li><nuxt-link to="/three+four">/three+four (<code>~/pages/three+four.vue</code>)</nuxt-link></li>
     </ol>
   </div>
 </template>
---
+++ pages/three+four.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Three Four</h1>
+  </div>
+</template>
