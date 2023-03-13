4216041844a13d2c16b565f629948973029257f9
lukaszflorczak <lukasz@florczak.dev>
2023-03-13 23:48:28 +0100

    chore: add `ol` list

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,11 @@
   <div>
     <h1>Click the link, then refresh the website</h1>
 
-    <nuxt-link :to="`/search?s=${phrase1}`">Single Word</nuxt-link>
-    <nuxt-link :to="`/search?s=${phrase2}`">Multiple Words with Spaces and Quotes</nuxt-link>
-    <nuxt-link :to="`/search?s=${phrase3}`">Phrase with `+` char</nuxt-link>
+    <ol>
+      <li><nuxt-link :to="`/search?s=${phrase1}`">Single Word</nuxt-link></li>
+      <li><nuxt-link :to="`/search?s=${phrase2}`">Multiple Words with Spaces and Quotes</nuxt-link></li>
+      <li><nuxt-link :to="`/search?s=${phrase3}`">Phrase with `+` char</nuxt-link></li>
+    </ol>
   </div>
 </template>
 
